35f8507da5c7b784a64dc19d008000009f8f0976
Buchenberger <gregory.buchenberger@charter.com>
2017-08-23 09:48:30 -0600

    editor position fix

--- modules/ui/src/components/SwaggerEditor/SwaggerEditor.js
+++ modules/ui/src/components/SwaggerEditor/SwaggerEditor.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import swaggerEditor, { plugins } from 'swagger-editor';
+import swaggerUI from "swagger-ui";
 import RaisedButton from 'material-ui/RaisedButton';
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
 import './SwaggerEditor.css';
@@ -18,40 +19,45 @@ class SwaggerEditor extends Component {
             dom_id: '#swagger-editor',
             url: 'http://localhost:9999/oas',
             layout: 'EditorLayout',
+            presets: [
+                swaggerUI.presets.apis
+            ],
             plugins: [
-            plugins.EditorPlugin,
-            plugins.ValidationPlugin,
-            plugins.ValidationApiPlugin,
-            plugins.LocalStoragePlugin,
-            plugins.EditorAutosuggestPlugin,
-            plugins.EditorAutosuggestSnippetsPlugin,
-            plugins.EditorAutosuggestKeywordsPlugin,
-            plugins.EditorAutosuggestRefsPlugin,
-            plugins.EditorAutosuggestOAS3KeywordsPlugin,
-            ]
+                plugins.EditorPlugin,
+                plugins.ValidationPlugin,
+                plugins.ValidationApiPlugin,
+                plugins.LocalStoragePlugin,
+                plugins.EditorAutosuggestPlugin,
+                plugins.EditorAutosuggestSnippetsPlugin,
+                plugins.EditorAutosuggestKeywordsPlugin,
+                plugins.EditorAutosuggestRefsPlugin,
+                plugins.EditorAutosuggestOAS3KeywordsPlugin,
+            ],
+            // components: {
+            //     EditorLayout
+            //   }
         })
     }
     constructor(props) {
         super(props);
         this.state = {
-            swagger: '',
         };
     }
 
     handleClickSave(event, index, value) {
-        console.log('spec', this.editor.specSelectors.specStr());
+        console.log('spec', localStorage.getItem('swagger-editor-content'));
     };
 
     handleClickReload(event, index, value) {
         console.log('reload clicked');
     };
-    
+
 
     render() {
         return <div id='swagger-editor-wrapper'>
             <Toolbar>
                 <ToolbarGroup firstChild={true}>
-                    <RaisedButton label="Save" primary={true} onClick={this.handleClickSave}/>
+                    <RaisedButton label="Save" primary={true} onClick={this.handleClickSave} />
                     <RaisedButton label="Reload" onClick={this.handleClickReload} />
                 </ToolbarGroup>
             </Toolbar>
